c048547a8cbf674300a8995965f437f91f37c0a0
Sowmya Nagayya <kkd.sowmya@gmail.com>
2021-11-16 20:59:56 -0600

    updated activity structure

--- src/App.js
+++ src/App.js
@@ -1,41 +1,6 @@
-import React, {useState} from "react";
-import Header from "./components/Header";
-import Project from "./components/Project";
-import Footer from "./components/Footer";
-import About from "./components/pages/About";
-import Contact from "./components/pages/Contact";
-import Portfolio from "./components/pages/Portfolio";
-import Resume from "./components/pages/Resume";
+import React from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
 
-export default function App() {
-    //useState to kepp track of which page to render
-    const [page, setPage] = useState("About");
+const App = () => <PortfolioContainer />;
 
-    //Determine which page to display
-    const renderPage;
-    switch (page) {
-        case "About":
-          renderPage = <About />;
-          break;
-        case "Portfolio":
-          renderPage = <Portfolio />;
-          break;
-        case "Resume":
-            renderPage = <Resume />;
-            break;
-        case "Contact":
-            renderPage = <Contact />;
-            break;
-        default:
-            renderPage = <About />;
-            break;        
-    }
-
-    return (
-        <div>
-            <Header setPage={setPage} setCurrentPage={page}/>
-            {renderPage}
-            <Footer />
-        </div>
-    );
-}
+export default App;
--- src/Components/pages/About.js
+++ src/Components/pages/About.js
@@ -1,36 +1,17 @@
-import React from "react";
+import React from 'react';
 
 export default function About() {
-    return(
-        <div className="container mt-5">
-            <div className="row justify-content-center">
-                <div className="col">
-                    <h1 className="text-center">About Me</h1>
-                </div>
-            </div>
-            
-            {/* My Image with Text */}
-            <div className="row mb-4 justify-content-center">
-                <div className="col-md-auto">
-                    <img src="" className="rounded-circle my-4 img-responsive center-block d-block mx-auto" alt="Sowmya Pic" title="Sowmya Nagayya"/>
-                </div>
-            </div>
-          
-            <div className="row mx-3">
-                <h6 className="my-5 text-center">My name is Sowmya and I'm a Computer Science Engineering graduate of the Vishweshwaraya University, Karnataka, India</h6>
-                <p className="mb-4">
-                    I'm currently taking a Web Development Bootcamp to continue to learn new technologies and build on my skillset while I apply to full-time roles.
-                </p>
-                <p className="mb-4">
-                    I'm currently taking a Web Development Bootcamp to continue to learn new technologies and build on my skillset while I apply to full-time roles.
-                </p>
-                <p className="mb-4">
-                    I'm currently taking a Web Development Bootcamp to continue to learn new technologies and build on my skillset while I apply to full-time roles.
-                </p>
-                <p className="mb-4">
-                    When I don't have VSCode pulled up, do exercise or play with my Boy.
-                </p>
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
+        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
+        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
+        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
+        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
+        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+      </p>
+    </div>
+  );
 }
