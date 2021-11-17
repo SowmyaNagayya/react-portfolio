c22b5bcf103e06fa81d1ca38f1e2811f354e3b87
Sowmya Nagayya <kkd.sowmya@gmail.com>
2021-11-17 13:07:42 -0600

    updating about me

--- src/Components/pages/About.js
+++ src/Components/pages/About.js
@@ -1,17 +1,34 @@
 import React from 'react';
+import myPic from "../../images/sowmya.jpg"
 
 export default function About() {
+
+  //style for profile image
+  const myPicStyle = {
+    maxHeight:'300px',
+    maxWidth:'300px'
+  }
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col">
+          <h1 className="text-center">About Me</h1>
+        </div>
+      </div>     
+      
+      <div class="row mb-4 justify-content-center">
+        <div className="col-md-auto">
+          <img src={myPic} className="rounded-circle my-4 img-responsive center-block d-block mx-auto" alt="Sowmya Pic" style={myPicStyle}/>
+        </div>
+      </div>
+
+      <div className="row mx-3">
+        <h6 className="my-5 text-center">My name is Sowmya Nagayya and I'm a Computer Science Engineer of the Vishveshwaraya University, Karnataka, India</h6>
+        <p className="mb-4">abc</p>
+        <p className="mb-4"></p>
+        <p className="mb-4"></p>
+        <p className="mb-4"></p>
+      </div>
     </div>
   );
 }
